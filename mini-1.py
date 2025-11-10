bdfb8280d876169a7da553e79eafd5f61015a2ae
christopherdade1974 <christopherdadejameson@gmail.com>
2025-11-09 22:49:07 -0800

    2nd mini project

---
+++ mini-1.py
@@ -0,0 +1,11 @@
+# About Me Program
+name = "Chris"
+age = 12
+hobby = "playing guitar"
+color = "blue"
+
+print(f"Hi, my name is {name}!")
+print(f"I’m {age} years old.")
+print(f"My favorite color is {color}.")
+print(f"I love {hobby}!")
+
